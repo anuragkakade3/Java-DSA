c9394a00e727190f15a30bf37cf5fc85c36a37eb
anurag <anurag.kakade20@gmail.com>
2024-07-04 08:36:45 +0530

    occurance

---
+++ Recursion/Occarence.java
@@ -0,0 +1,17 @@
+public class Occarence {
+    public static int isoccarence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        if (arr[i] == key) {
+            return i;
+        }
+        return isoccarence(arr, key, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, 3, 6, 9, 5, 10, 2, 5, 3 };
+        System.out.println(isoccarence(arr, 5, 0));
+    }
+
+}
