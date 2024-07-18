fbb1d90fe6c5790d05c73e44d650bd5e97d51f95
anurag <anurag.kakade20@gmail.com>
2024-07-18 20:36:36 +0530

    feat: create Arraylist.java

---
+++ ArrayList/Arraylist.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Arraylist {
+
+    public static void swap(ArrayList<Integer>list, int indx1, int indx2){
+        int temp=list.get(indx1);
+        list.set(indx1, list.get(indx2));
+        list.set(indx2, temp);
+    }
+
+    public static void main(String[]args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(9);
+        list.add(4);
+        list.add(5);
+        System.out.println(list); // prints: [1, 2, 3, 4]
+
+        // Reverse array
+
+        // for(int i=list.size()-1;i>=0;i--){
+        //     System.out.print(list.get(i) + " "); // prints: 5 4 3 2 1
+        // }
+
+        // Find Max
+
+        // int max= Integer.MIN_VALUE;
+        // for(int i=0;i<list.size();i++){
+        //     if(max<list.get(i)){
+        //         max=list.get(i);
+        //     }
+        // }
+        // System.out.println("Max No. is: " + max);
+
+        // Swap
+        
+        int indx1=1, indx2=3;
+        swap(list, indx1, indx2);
+
+
+    }
+}
