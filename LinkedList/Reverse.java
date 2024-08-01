c709167bda244cab0aaf9764d018d9c18d59976d
anurag <anurag.kakade20@gmail.com>
2024-08-01 07:45:36 +0530

    reverse LL

---
+++ LinkedList/Reverse.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+// File name & Class name save as a LinkedList to run the code.
+public class Reverse {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        size++;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        size++;
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public static void print() {
+        if (head == null) {
+            System.out.println("LL is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public void add(int idx, int data){
+        if(idx==0){
+            addFirst(data);
+            return;    
+        }
+        size++;
+        Node newNode=new Node(data);
+        Node temp=head;
+        int i=0;
+
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    public void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while (curr!=null) {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3); 
+        ll.addLast(4);
+        ll.add(3,4);
+        ll.addLast(5);
+        ll.print();
+        ll.reverse();
+        ll.print();
+    }
+}
+// O/P:- 1->2->3->4->4->5->Null
+//       5->4->4->3->2->1->Null
+
