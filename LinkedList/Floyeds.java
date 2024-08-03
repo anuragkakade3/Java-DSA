871de4d05f544e851d508adff9100eb612136733
anurag <anurag.kakade20@gmail.com>
2024-08-03 06:32:11 +0530

    Added new Java launch configuration "Floyeds" to launch.json.

---
+++ LinkedList/Floyeds.java
@@ -0,0 +1,63 @@
+public class Floyeds {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    
+
+    public static void print() {
+        if (head == null) {
+            System.out.println("LL is Empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public static boolean isCyle(){
+        Node slow=head;
+        Node fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;// +1
+            fast=fast.next.next; // +2
+
+            if(slow==fast){
+                return true;
+            }
+
+        }
+        return false;
+
+
+    }
+    
+
+    
+    
+
+    public static void main(String[] args) {
+        head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);;
+        head.next.next.next=head;
+        System.out.println(isCyle());
+
+        
+    }
+}
+// O/P:- true
