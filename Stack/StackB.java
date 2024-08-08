280259ad16a6f74af65042c1c52cd36cce7789dc
anurag <anurag.kakade20@gmail.com>
2024-08-08 14:51:50 +0530

    Added a new Java launch configuration for "StackB" in launch.json.

---
+++ Stack/StackB.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class StackB {
+    
+    public static void pushAtBottom(Stack<Integer>stack,int data){
+        if(stack.isEmpty()){
+            stack.push(data);
+            return;
+        }
+
+        int top=stack.pop();
+        pushAtBottom(stack, data);
+        stack.push(top);
+    }
+    
+    public static void main(String[] args) {
+        Stack<Integer>stack=new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        pushAtBottom(stack, 4);
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+
+    
+    }
+}
