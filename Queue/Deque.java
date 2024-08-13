cf8a883842686ca32f5fd3642b1c12ec5b1b9fba
anurag <anurag.kakade20@gmail.com>
2024-08-13 08:51:04 +0530

    dequeue

---
+++ Queue/Deque.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Deque {
+    public static void main(String[] args) {
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1);
+        d.addFirst(1);
+        System.out.println(d);
+        d.removeFirst();
+        d.addLast(3);
+        d.addLast(4);
+        System.out.println(d);
+        d.removeFirst();
+        d.removeLast();
+        System.out.println(d);
+
+        
+        
+
+    }
+}
